ce1157841e7dba5cae813cae4b26c0204b117470
NyxTRXWolfe <126172007+NyxTRXWolfe@users.noreply.github.com>
2023-02-24 10:50:17 -0500

    Add files via upload

---
+++ Windows OS Corruption Fix.ps1
@@ -0,0 +1,11 @@
+﻿
+Start-Process -Filepath "${env:Windir}\system32\SFC.exe" -ArgumentList '/scannow' -verb runas
+
+Start-Process -Filepath "${env:windir}\system32\WindowsPowerShell\v1.0\powershell.exe" -verb runas -argumentlist 'DISM /Online /Cleanup-Image /RestoreHealth' | sfc /scannow -verb runas
+
+
+for ($i = 1; $i -le 100; $i++ ) {
+Write-Progress -Activity "Fixing OS please standby..." -status "$i% Complete;" -PercentComplete $i
+Start-sleep -Milliseconds 250
+    
+}
