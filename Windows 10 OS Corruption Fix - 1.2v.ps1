ab7413b8afc282a129638b323563bf3039ae2e13
NyxTRXWolfe <126172007+NyxTRXWolfe@users.noreply.github.com>
2023-03-09 10:44:56 -0500

    Add files via upload

---
+++ Windows 10 OS Corruption Fix - 1.2v.ps1
@@ -0,0 +1,11 @@
+﻿# Save the current execution policy
+$currentPolicy = Get-ExecutionPolicy
+
+# Set the execution policy to RemoteSigned for the duration of the script
+Set-ExecutionPolicy RemoteSigned -Scope Process
+
+# Run the commands
+Start-Process -FilePath "${env:windir}\system32\WindowsPowerShell\v1.0\powershell.exe" -Verb RunAs -ArgumentList '-Command', 'DISM /Online /Cleanup-Image /RestoreHealth ; sfc /scannow'
+
+# Restore the original execution policy
+Set-ExecutionPolicy $currentPolicy -Scope Process
